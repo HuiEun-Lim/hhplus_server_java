125b3b70139b52d482a3446edf2b4b2471ad8366
HuiEun-Lim <heyho930@gmail.com>
2025-01-09 02:22:14 +0900

    FEAT: 포인트 facade 생성 + 단위테스트

---
+++ src/main/java/kr/hhplus/be/server/application/point/facade/PointFacade.java
@@ -0,0 +1,44 @@
+package kr.hhplus.be.server.application.point.facade;
+
+import kr.hhplus.be.server.application.point.dto.PointFacadeResponse;
+import kr.hhplus.be.server.domain.point.dto.UserPointResult;
+import kr.hhplus.be.server.domain.point.service.PointService;
+import kr.hhplus.be.server.domain.user.dto.UserResult;
+import kr.hhplus.be.server.domain.user.service.UserService;
+import kr.hhplus.be.server.support.util.point.PointValidationUtils;
+import kr.hhplus.be.server.support.util.user.UserValidationUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class PointFacade {
+
+    private final PointService pointService;
+    private final UserService userService;
+
+    @Transactional
+    public PointFacadeResponse chargeUserPoint(Long userId, Long amount){
+        // 사용자 정보 검증
+        UserResult user = userService.getUserByUserId(userId);
+
+        // 포인트 충전 처리
+        UserPointResult pointResult = pointService.chargeUserPoint(userId, amount);
+
+        return PointFacadeResponse.toResponse(pointResult, user);
+
+    }
+
+    @Transactional(readOnly = true)
+    public PointFacadeResponse getUserPoint(Long userId){
+        // 사용자 정보 검증
+        UserResult user = userService.getUserByUserId(userId);
+
+        // 포인트 조회
+        UserPointResult pointResult = pointService.getUserPoint(userId);
+
+        return PointFacadeResponse.toResponse(pointResult, user);
+
+    }
+}
