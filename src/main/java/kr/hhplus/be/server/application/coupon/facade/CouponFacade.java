b4d3b1bab1c24196ebbf19b72098446f5287b277
HuiEun-Lim <heyho930@gmail.com>
2025-01-09 18:22:58 +0900

    FEAT: 쿠폰 facade 생성 + 단위테스트

---
+++ src/main/java/kr/hhplus/be/server/application/coupon/facade/CouponFacade.java
@@ -0,0 +1,48 @@
+package kr.hhplus.be.server.application.coupon.facade;
+
+import kr.hhplus.be.server.application.coupon.dto.CouponIssuanceFacadeResponse;
+import kr.hhplus.be.server.domain.coupon.dto.CouponIssuanceResult;
+import kr.hhplus.be.server.domain.coupon.service.CouponService;
+import kr.hhplus.be.server.domain.user.dto.UserResult;
+import kr.hhplus.be.server.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component("couponFacade")
+@RequiredArgsConstructor
+public class CouponFacade {
+
+    private final CouponService couponService;
+    private final UserService userService;
+
+    @Transactional
+    public CouponIssuanceFacadeResponse issueCoupon(Long userId, Long couponId) {
+        // 사용자 정보 검증
+        UserResult user = userService.getUserByUserId(userId);
+
+        // 쿠폰 발급 처리
+        CouponIssuanceResult issuedCoupon = couponService.issueCoupon(userId, couponId);
+
+        return CouponIssuanceFacadeResponse.toFacadeResponse(issuedCoupon, user);
+    }
+
+    @Transactional(readOnly = true)
+    public List<CouponIssuanceFacadeResponse> userIssuedCoupons(Long userId) {
+        // 사용자 정보 검증
+        UserResult user = userService.getUserByUserId(userId);
+
+        // 사용자 쿠폰 목록 조회
+        List<CouponIssuanceResult> issuanceList = couponService.userIssuedCouponList(userId);
+
+        return issuanceList.stream()
+                .map(issuance -> {
+                    return CouponIssuanceFacadeResponse.toFacadeResponse(issuance, user);
+                })
+                .collect(Collectors.toList());
+    }
+
+}
