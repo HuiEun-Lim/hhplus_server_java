a3d10b6b7ccfb6a6ff7e4b930963570d35d172af
HuiEun-Lim <heyho930@gmail.com>
2025-01-10 03:54:05 +0900

    CHORE: QueryDsl 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,12 @@ dependencies {
 	compileOnly ("org.projectlombok:lombok")
 	annotationProcessor ("org.projectlombok:lombok")
 	implementation(kotlin("stdlib-jdk8"))
+
+	// QueryDSL
+	implementation ("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	annotationProcessor ("com.querydsl:querydsl-apt:5.0.0:jakarta")
+	annotationProcessor ("jakarta.annotation:jakarta.annotation-api")
+	annotationProcessor ("jakarta.persistence:jakarta.persistence-api")
 }
 
 tasks.withType<Test> {
