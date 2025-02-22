c0b442bade5846abee864c4a0c32f2bf1d77b419
HuiEun-Lim <heyho930@gmail.com>
2025-02-22 14:25:13 +0900

    FEAT: 주문 아웃박스 배치 수정

--- src/main/java/kr/hhplus/be/server/domain/order/outbox/service/OrderOutboxBatchService.java
+++ src/main/java/kr/hhplus/be/server/domain/order/outbox/service/OrderOutboxBatchService.java
@@ -3,6 +3,7 @@ package kr.hhplus.be.server.domain.order.outbox.service;
 import kr.hhplus.be.server.domain.order.outbox.entity.OrderOutbox;
 import kr.hhplus.be.server.domain.order.outbox.enums.OrderOutboxStatus;
 import kr.hhplus.be.server.domain.order.outbox.repository.OrderOutboxRepository;
+import kr.hhplus.be.server.infrastructure.kafka.order.OrderKafkaConstants;
 import kr.hhplus.be.server.infrastructure.kafka.order.OrderKafkaProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,17 +20,13 @@ public class OrderOutboxBatchService {
     private final OrderOutboxRepository orderOutboxRepository;
     private final OrderKafkaProducer OrderKafkaProducer;
 
-    private final String ORDER_COMPLETED = "order-completed";
-
     @Scheduled(fixedDelay = 300000)
     public void retryFailedOrderCompletedMessages() {
         List<OrderOutbox> failedMessages = orderOutboxRepository.findByStatusAndCreatedDate(OrderOutboxStatus.SAVE, LocalDateTime.now().minusMinutes(5));
 
         for (OrderOutbox outbox : failedMessages) {
             try {
-                OrderKafkaProducer.send(ORDER_COMPLETED, outbox.getMessageId(), outbox.getPayload());
-                outbox.updateStatus(OrderOutboxStatus.SUCCESS);
-                orderOutboxRepository.save(outbox);
+                OrderKafkaProducer.send(OrderKafkaConstants.ORDER_COMPLETED, outbox.getMessageId(), outbox.getPayload());
                 log.info("Kafka 재발송 성공: 주문 ID = {}", outbox.getMessageId());
             } catch (Exception e) {
                 log.error("Kafka 재발송 실패: 주문 ID = {}", outbox.getMessageId(), e);
