4d55fede990207a1fa873cb0da32a9841bd754ba
HuiEun-Lim <heyho930@gmail.com>
2025-02-23 22:07:28 +0900

    FEAT: Kafka 테스트 토픽 상수로 변경

--- src/test/java/kr/hhplus/be/server/infrastructure/kafka/KafkaIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/infrastructure/kafka/KafkaIntegrationTest.java
@@ -4,12 +4,14 @@ import kr.hhplus.be.server.application.IntegrationTestSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
+@Component
 class KafkaIntegrationTest extends IntegrationTestSupport {
 
     @Autowired
@@ -18,14 +20,15 @@ class KafkaIntegrationTest extends IntegrationTestSupport {
     @Autowired
     private TestConsumer consumer;
 
+    private static final String TOPIC = "test-topic";
+
     @Test
     @DisplayName("Kafka 테스트")
-    public void kafkaSendAndReceiveTest() throws InterruptedException {
+    void kafkaSendAndReceiveTest() throws InterruptedException {
         // given
         int cnt = 5;
-        String topic = "test-topic";
         for (int i = 0; i < cnt; i++) {
-            producer.send(topic, "MESSAGE " + (i + 1));
+            producer.send(TOPIC, "MESSAGE " + (i + 1));
         }
 
         // when
